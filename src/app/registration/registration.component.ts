325f61821fc12164fe281650e837b65812a7d154
fathima890 <fathimarafa890@gmail.com>
2021-08-17 07:45:12 +0530

    initial commit

---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent  {
+
+
+  submit(login:any)
+  {
+      console.log("Form submitted",login);
+  }
+  
+
+}
